a53974a38de2f03fa42e3ff73c176427c5e8cdf9
lucas.149917 <lucas.149917@L-1-17-03--10.satc.edu.br>
2018-03-08 16:26:48 -0300

    Projeto Escola foi criado as classes: Main, Aluno, Disciplina, Pessoa, Professor e foi utilizados os métodos para caucular salario e idade.

--- src/br/com/lucas/objetos/Aluno.java
+++ src/br/com/lucas/objetos/Aluno.java
@@ -16,4 +16,43 @@ public class Aluno {
    private Integer matricula;
    private Date dataMatricula;
    private ArrayList disciplinas;
+
+    public Aluno(Integer matricula, Date dataMatricula, ArrayList disciplinas) {
+        this.matricula = matricula;
+        this.dataMatricula = dataMatricula;
+        this.disciplinas = disciplinas;
+    }
+
+    @Override
+    public String toString() {
+        return "Aluno{" + "matricula=" + matricula + ", dataMatricula=" + dataMatricula + ", disciplinas=" + disciplinas + '}';
+    }
+   
+   
+
+    public Integer getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(Integer matricula) {
+        this.matricula = matricula;
+    }
+
+    public Date getDataMatricula() {
+        return dataMatricula;
+    }
+
+    public void setDataMatricula(Date dataMatricula) {
+        this.dataMatricula = dataMatricula;
+    }
+
+    public ArrayList getDisciplinas() {
+        return disciplinas;
+    }
+
+    public void setDisciplinas(ArrayList disciplinas) {
+        this.disciplinas = disciplinas;
+    }
+   
+   
 }
